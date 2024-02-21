468e2620bfd289138c49f3d665559e39fd6c821a
tanvir35web <islammdtanvirul@gmail.com>
2024-02-21 16:23:33 +0600

    animation done on seconds position

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
     return (
         <>
             <div className="w-full h-[100vh] flex items-center justify-center gap-10">
-                <CountDown duration={14 * 25 * 40 * 1000} />
                 <CountDown duration={20 * 44 * 62 * 1000} />
             </div>
         </>
--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./countDown.css";
 
 const CountDown = ({ duration }) => {
     const [time, setTime] = useState(duration);
@@ -14,21 +15,40 @@ const CountDown = ({ duration }) => {
         const totalMinutes = parseInt(Math.floor(totalSeconds / 60));
         const totalHours = parseInt(Math.floor(totalMinutes / 60));
 
-        const seconds = parseInt(totalSeconds % 60);
-        const minutes = parseInt(totalMinutes % 60);
+        const seconds =
+            parseInt(totalSeconds % 60) < 10
+                ? "0" + parseInt(totalSeconds % 60)
+                : parseInt(totalSeconds % 60);
+        const minutes =
+            parseInt(totalMinutes % 60) < 10
+                ? "0" + parseInt(totalMinutes % 60)
+                : parseInt(totalMinutes % 60);
         const hours = parseInt(totalHours % 24);
 
-        return `${hours < 10 ? "0" + hours : hours} : ${
-            minutes < 10 ? "0" + minutes : minutes
-        } : ${seconds < 10 ? "0" + seconds : seconds}`;
+        const s1 = Math.floor(seconds / 10);
+        const s2 = Math.floor(seconds % 10);
+
+        return { seconds, minutes, hours, s1, s2 };
     };
 
     const formattedTime = getFormattedTime(time);
 
     return (
-        <div className="w-96 h-48 bg-gray-100 rounded-lg flex flex-col items-center justify-center gap-2 shadow-lg">
+        <div className="w-[30rem] h-48 bg-gray-100 rounded-lg flex flex-col items-center justify-center gap-2 shadow-lg">
             <h1 className="font-bold text-2xl">Count Down</h1>
-            <h3 className="font-bold text-5xl"> {formattedTime} </h3>
+
+            <div className=" h-12 flex items-center p-2 gap-4  font-bold text-5xl  justify-center overflow-hidden relative">
+                <h3> {formattedTime.hours} </h3>
+                <h3>:</h3>
+                <h3> {formattedTime.minutes} </h3>
+                <h3>:</h3>
+                <h3 className="move"> {formattedTime.seconds} </h3>
+                {/* <h3>:</h3>
+                <h3 className={formattedTime.s1 && "move"}>
+                    {formattedTime.s1}
+                </h3>
+                <h3 className="move">{formattedTime.s2}</h3> */}
+            </div>
         </div>
     );
 };
