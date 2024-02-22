fcc80a6bbb11e73c9f5fc735a729afa8f61a698f
tanvir35web <islammdtanvirul@gmail.com>
2024-02-22 09:15:36 +0600

    count slider component added

---
+++ src/components/CountSlide.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CountSlide = () => {
+    return (
+        <>
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+        </>
+    );
+};
+
+export default CountSlide;
